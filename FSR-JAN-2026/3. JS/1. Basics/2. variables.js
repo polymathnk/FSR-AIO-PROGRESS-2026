9518444f3a28c7129590cf950f6bcf3ee2313605
polymathnk <polymathnk@gmail.com>
2026-02-01 10:24:40 +0530

    variables in js

---
+++ FSR-JAN-2026/3. JS/1. Basics/2. variables.js
@@ -0,0 +1,37 @@
+// ways to declare a variable
+// var, let, const 
+
+let firstName = 'Nitin'
+let lastName = 'Kumar'
+console.log(firstName, lastName);
+
+
+let age = 20;
+console.log(age);
+
+// Naming conventions : 
+//  - Only letters, numbers, underscores, and dollar signs
+//  - cant start with a number
+
+// Multi-Word Formatting for variables
+//  firstName = camelCase
+//  first_name = underscore
+//  FirstName = PascalCase
+// firstname = lowercase
+
+//  Re-assigning variables 
+
+age = 24;
+console.log(age)
+
+let score; 
+score =  10;
+
+console.log(score);
+
+const c = 100;
+
+c = 200;
+
+console.log(c);
+
