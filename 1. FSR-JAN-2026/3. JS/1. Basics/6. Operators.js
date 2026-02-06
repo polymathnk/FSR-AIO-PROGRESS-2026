da3544ce28ed2b73a37aa32a70459e6552b78cb5
polymathnk <polymathnk@gmail.com>
2026-02-06 14:21:16 +0530

    operators in js

---
+++ 1. FSR-JAN-2026/3. JS/1. Basics/6. Operators.js
@@ -0,0 +1,64 @@
+// 1. Arithematic operators
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenation
+
+x = 'hello' + ' ' + 'World';
+
+// Exponents
+
+x = 2**4;
+
+//  Increment
+
+x = 1;
+//  x = x + 1;
+x ++;
+
+//  Decrement
+
+x = 1;
+//  x = x - 1;
+x --;
+
+//  2. Assignment
+
+x = 10;
+
+x += 10;
+x -= 10;
+x *= 10;
+x /= 10;
+x %= 10;
+
+
+//  3. Comparsion
+
+x = 2 === 2; // loose comparison
+
+x = 4 === '4'; // tight comparsion
+
+x = 2 != 2; // loose not equals
+
+x = 2 !== 2; // tight not equals
+
+x = 2 > 1; // greater than
+
+x = 2 >= 1; // greater than or equals to
+
+x = 2 < 6; // less than
+
+x = 2 <= 1; // less than or equals to
+
+console.log(x);
+
+//  
+
+
